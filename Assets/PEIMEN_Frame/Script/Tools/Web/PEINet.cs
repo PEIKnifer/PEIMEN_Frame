413fc93f5a92b1e154fe3895da6d619b66bead18
546964794 <546964794@qq.com>
2019-10-08 14:53:27 +0800

    add web test control

---
+++ Assets/PEIMEN_Frame/Script/Tools/Web/PEINet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PEINet : PEINet_Origin
+{
+    static PEINet instance;
+    public static PEINet Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GetIns<PEINet>();
+            }
+            return instance;
+        }
+    }
+}
